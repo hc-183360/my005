3f80c07a12cacea44417ecaf60a09bda0a77e7fd
hc-183360 <810411739@qq.com>
2021-08-06 12:17:54 +0800

    1

---
+++ workcode/micr-dataservice/src/main/java/com/bjpowernode/licai/service/impl/newclass.java
@@ -0,0 +1,8 @@
+package com.bjpowernode.licai.service.impl;
+
+public class newclass {
+    public static void main(String[] args) {
+        System.out.println("" +
+                "12345");
+    }
+}
